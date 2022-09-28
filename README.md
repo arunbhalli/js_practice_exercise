49a9ede50d22bf0c0678b7aab104ed656cc668ec
arunbhalli <tomarbhalli@gmail.com>
2022-09-28 09:43:24 +0200

    adds new array of square of old arr

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 <h1> Exercise<h1>
 
-<img src="/home/arunsingh/projects/js_exercise/exercise/Screenshot 2022-09-28 091301.png" alt="Alt text" title="Optional title">
+<img src="exercise/Screenshot 2022-09-28 091301.png" alt="Alt text" title="Optional title">
 
 
--- exercise/userarray.js
+++ exercise/userarray.js
@@ -1,15 +1,13 @@
-
-// Quetion 1 
+// Quetion 1
 /*var arr = [1,2,3,4,5,6,7];
 var size = prompt('Enter array Element ');
 size = Number.parseInt(size)
 arr.push(size)
 console.log(arr);*/
 
-
 // quetion 2
 
-var arr = [1, 20, 3, 4, 50, 6, 70];
+let arr = [1, 20, 3, 4, 50, 6, 70];
 let size;
 /*do {
   size = prompt('Enter array Element ');
@@ -17,8 +15,17 @@ let size;
   arr.push(size);
 } while (size != 0);
 console.log(arr);*/
+
 // Quetion3 filter the array
-let n =arr.filter((a)=>{
+
+/*let n =arr.filter((a)=>{
   return a%10 === 0;
 })
-console.log(n)
+console.log(n)*/
+
+// Quetion4 new array of square
+
+let n = arr.map((a) => {
+  return a * a;
+});
+console.log(n);
