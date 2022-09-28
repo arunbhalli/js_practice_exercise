5a4b09f061b2141537317f68724849712a283bf6
arunbhalli <tomarbhalli@gmail.com>
2022-09-28 08:55:03 +0200

    adds functionality for filtyering an array

--- exercise/userarray.js
+++ exercise/userarray.js
@@ -9,11 +9,16 @@ console.log(arr);*/
 
 // quetion 2
 
-var arr = [1, 2, 3, 4, 5, 6, 7];
+var arr = [1, 20, 3, 4, 50, 6, 70];
 let size;
-do {
+/*do {
   size = prompt('Enter array Element ');
   size = Number.parseInt(size);
   arr.push(size);
 } while (size != 0);
-console.log(arr);
+console.log(arr);*/
+// Quetion3 filter the array
+let n =arr.filter((a)=>{
+  return a%10 === 0;
+})
+console.log(n)
