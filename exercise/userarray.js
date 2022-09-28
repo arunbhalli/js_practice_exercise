acab768eb5938ae0189091d31168fd7a01a230e9
arunbhalli <tomarbhalli@gmail.com>
2022-09-28 08:49:00 +0200

    solution for quetion 2

--- exercise/userarray.js
+++ exercise/userarray.js
@@ -1,5 +1,19 @@
-var arr = [1,2,3,4,5,6,7];
+
+// Quetion 1 
+/*var arr = [1,2,3,4,5,6,7];
 var size = prompt('Enter array Element ');
 size = Number.parseInt(size)
 arr.push(size)
+console.log(arr);*/
+
+
+// quetion 2
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+let size;
+do {
+  size = prompt('Enter array Element ');
+  size = Number.parseInt(size);
+  arr.push(size);
+} while (size != 0);
 console.log(arr);
